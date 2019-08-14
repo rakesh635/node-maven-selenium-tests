86966d3b7b7d00f6794efa78eaae86ea1d7e880e
Paweł Kapała <bylek77@gmail.com>
2019-08-14 13:11:59 +0000

    Changes to index.js

--- front/index.js
+++ front/index.js
@@ -1,6 +1,20 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-  res.write('Hello Buddy!');
+  res.write(`
+	<!doctype html>
+	<html lang="en">
+	<head>
+	  <meta charset="utf-8">
+	
+	  <title>Hello Buddy!</title>
+	  <meta name="description" content="Hello Buddy!">
+	</head>
+	
+	<body>
+		Hello Buddy!
+	</body>
+	</html>
+  `);
   res.end();
 }).listen(80);
